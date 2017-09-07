8b99da438f686086e22fad8ef6642c99a98d561b
krzysztof dubowski <krzysztof.dubowski@gmail.com>
2017-09-07 19:28:33 +0200

    "Gotoewa lista"

---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_list_")
+     */
+    public function listAction(Request $request){
+        //die('lista');
+        return $this->render('todo/index.html.twig');
+        
+    }
+    
+     /**
+     * @Route("/todos/create", name="todo_create")
+     */
+    public function createAction(Request $request){
+        //die('lista');
+        return $this->render('todo/create.html.twig');
+        
+    }
+    
+     /**
+     * @Route("/todos/edit/{id}", name="todo_edit")
+     */
+    public function editAction($id,Request $request){
+        //die('lista');
+        return $this->render('todo/edit.html.twig');
+        
+    }
+    
+     /**
+     * @Route("/todos/details/{id}", name="todo_details")
+     */
+    public function detailsAction($id){
+       
+        return $this->render('todo/details.html.twig');
+        
+    }
+    
+ }
+
